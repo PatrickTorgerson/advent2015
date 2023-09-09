d8f23cbcd3e3292218f1293aa18fa22e6ba9eb3e
Patrick Torgerson <TorgersonPatrickS@gmail.com>
2023-09-09 12:50:12 -0700

    Implement day 1 part 1

--- src/solutions/day1.zig
+++ src/solutions/day1.zig
@@ -24,10 +24,36 @@ pub fn solve(allocator: std.mem.Allocator, writer: *Writer) anyerror!void {
     try benchmark(allocator, writer, part2);
 }
 
-/// PART 1 DESCRIPTION
-fn part1(allocator: std.mem.Allocator) ![]const u8 {
+/// Santa is trying to deliver presents in a large apartment building, but he can't find the right
+/// floor - the directions he got are a little confusing. He starts on the ground floor (floor 0)
+/// and then follows the instructions one character at a time.
+///
+/// An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ),
+/// means he should go down one floor.
+///
+/// The apartment building is very tall, and the basement is very deep; he will never find the
+/// top or bottom floors.
+///
+/// For example:
+///
+///   - (()) and ()() both result in floor 0.
+///   - ((( and (()(()( both result in floor 3.
+///   - ))((((( also results in floor 3.
+///   - ()) and ))( both result in floor -1 (the first basement level).
+///   - ))) and )())()) both result in floor -3.
+///
+/// To what floor do the instructions take Santa?
+fn part1(allocator: std.mem.Allocator) !i64 {
     _ = allocator;
-    return "not implemented";
+    var floor: i64 = 0;
+    for (input) |dir| {
+        floor += switch (dir) {
+            '(' => 1,
+            ')' => -1,
+            else => 0,
+        };
+    }
+    return floor;
 }
 
 /// PART 2 DESCRIPTION
